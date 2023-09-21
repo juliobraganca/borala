5ef4117a54288ed63ab10499f0f2aa9c00bc9c08
Rober <devsobarzo@gmail.com>
2023-09-21 15:15:57 -0300

    City & Date View added.

--- borala/borala/ViewController.swift
+++ borala/borala/ViewController.swift
@@ -9,12 +9,68 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    private let collectionView = UICollectionView(
+        frame: .zero,
+        collectionViewLayout: UICollectionViewFlowLayout()
+    )
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        collectionView.register(
+            CitiesCollectionViewCell.self,
+            forCellWithReuseIdentifier: CitiesCollectionViewCell.identifier)
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        view.addSubview(collectionView)
         view.backgroundColor = .purple
     }
+}
 
+extension ViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 21
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: CitiesCollectionViewCell.identifier, for: indexPath)
+        return cell
+    }
+}
+
+extension ViewController: UICollectionViewDelegate {
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        collectionView.frame = view.bounds
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let selectedIndexPath = indexPath
+        print(indexPath)
+        
+        let AddCityViewController = UIStoryboard(name: "AddCityViewController", bundle: nil).instantiateViewController(withIdentifier: "AddCityViewController") as! AddCityViewController
+        self.navigationController?.pushViewController(AddCityViewController, animated: true)
+    }
 
 }
 
+
+extension ViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(
+            width: (view.frame.size.width/3)-3,
+            height: (view.frame.size.width/3)-3
+            )
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 1
+    }
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return UIEdgeInsets(top: 1, left: 1, bottom: 1, right: 1)
+    }
+    
+}
