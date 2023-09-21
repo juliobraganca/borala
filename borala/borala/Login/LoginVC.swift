b860b91167e73b05920035a33c9f27a95ae3dbb1
Luana Mattana Damin <ludamin@gmail.com>
2023-09-21 13:12:28 -0300

    configuração outLet elementos

--- borala/borala/Login/LoginVC.swift
+++ borala/borala/Login/LoginVC.swift
@@ -8,11 +8,34 @@
 import UIKit
 
 class LoginVC: UIViewController {
-
+    
+    @IBOutlet weak var emailTextField: UITextField!
+    
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    @IBOutlet weak var registerGreyButton: UIButton!
+    
+    @IBOutlet weak var forgotPasswordGreyButton: UIButton!
+    
+    @IBOutlet weak var enterButton: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        configElements()
     }
 
+    func configElements() {
+        emailTextField.placeholder = "Digite seu e-mail"
+        passwordTextField.placeholder = "Digite sua senha"
+        
+        registerGreyButton.setTitle("Cadastre-se", for: .normal
+        )
+        
+        forgotPasswordGreyButton.setTitle("Esqueci a senha", for: .normal)
+        
+        enterButton.setTitle("Entrar", for: .normal)
+    }
 
 }
 
