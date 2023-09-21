e72c1cdaaef71743dcd103a91f64e52c0329e316
Julio <jcbbraganca@gmail.com>
2023-09-21 11:14:47 -0300

    Constants creation

---
+++ borala/borala/Utilities/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  borala
+//
+//  Created by Julio Braganca on 21/09/23.
+//
+
+import UIKit
+
+enum Images {
+    static let background   = UIImage(named: "background")
+    static let logo         = UIImage(named: "logoBorala")
+}
