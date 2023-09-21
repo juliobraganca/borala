0700ba0f7574ffa2d19632d9510713a1d4b61ffe
Julio <jcbbraganca@gmail.com>
2023-09-21 11:15:18 -0300

    UIView + UITextField + UIButton extensions creation

---
+++ borala/borala/Extensions/UITextField+Ext.swift
@@ -0,0 +1,33 @@
+//
+//  UITextField+Ext.swift
+//  borala
+//
+//  Created by Julio Braganca on 21/09/23.
+//
+
+import UIKit
+
+extension UITextField {
+    
+    func isEditingTextField() {
+        self.layer.borderColor = UIColor.cyan.cgColor
+        self.layer.borderWidth = 1.5
+    }
+    
+    
+    func setupTextField(placeholder: String, delegate: UITextFieldDelegate) {
+        self.placeholder = placeholder
+        self.delegate = delegate
+        self.layer.borderColor = UIColor.red.cgColor
+        self.layer.borderWidth = 1.5
+    }
+    
+    
+    func isTextFieldEmpty() {
+        if self.hasText {
+            self.layer.borderColor = UIColor.orange.cgColor
+        } else {
+            self.layer.borderColor = UIColor.red.cgColor
+        }
+    }
+}
