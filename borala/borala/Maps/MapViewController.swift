b15da0105eac303110c65d15fef7c76827000b8a
Rober <devsobarzo@gmail.com>
2023-09-21 11:57:02 -0300

    Map's view added.

--- borala/borala/Maps/MapViewController.swift
+++ borala/borala/Maps/MapViewController.swift
@@ -2,28 +2,39 @@
 //  MapViewController.swift
 //  borala
 //
-//  Created by Rober on 19/09/23.
+//  Created by Rober on 21/09/23.
 //
 
 import UIKit
+import MapKit
 
 class MapViewController: UIViewController {
-
+    
+    let mapView = MKMapView()
+    let coordinate = CLLocationCoordinate2D(latitude: -22.988495, longitude: -43.192742)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = .yellow
+        configureMap()
+    }
+}
 
-        // Do any additional setup after loading the view.
+extension MapViewController {
+    private func configureMap() {
+        view.addSubview(mapView)
+        mapView.frame = view.bounds
+        let span = MKCoordinateSpan(latitudeDelta: 0.5, longitudeDelta: 0.5)
+        let region = MKCoordinateRegion(center: coordinate, span: span)
+        mapView.setRegion(region, animated: true)
+        
+        addPin()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    private func addPin() {
+        let pin = MKPointAnnotation()
+        pin.coordinate = coordinate
+        pin.title = "Here you are"
+        mapView.addAnnotations([pin])
     }
-    */
-
 }
