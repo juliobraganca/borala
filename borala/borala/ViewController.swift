0378b4b9818221d280febc6744713954d08f3a8d
Eduardo <eduardo07assis@gmail.com>
2023-09-26 03:12:13 -0300

    inserido ligações.

--- borala/borala/ViewController.swift
+++ borala/borala/ViewController.swift
@@ -9,8 +9,29 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    
     @IBOutlet weak var backgroundImageView: UIImageView!
+    
+    @IBOutlet weak var imageProfileImageView: UIImageView!
+    
+    @IBOutlet weak var nameTextField: UITextField!
+    
+    @IBOutlet weak var emailTextField: UITextField!
+    
+    @IBOutlet weak var dateOfVBirthTextField: UITextField!
+    
+    @IBOutlet weak var countryTextField: UITextField!
+    
+    @IBOutlet weak var CityTextField: UITextField!
+    
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    @IBOutlet weak var backButton: UIButton!
+    
+    @IBOutlet weak var saveButton: UIButton!
+    
+    @IBOutlet weak var editProfileButton: UIButton!
+    
+    @IBOutlet weak var editImageProfileButton: UIButton!
     
     
     
@@ -20,7 +41,6 @@ class ViewController: UIViewController {
         
         
         func configElementsBackground(){
-            
             backgroundImageView.image = UIImage(named: "background")
             backgroundImageView.contentMode = .scaleAspectFill
             
