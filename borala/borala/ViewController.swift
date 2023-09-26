773ade7254e607164701d9dc19d7fd99836d1da2
Eduardo <eduardo07assis@gmail.com>
2023-09-26 01:54:28 -0300

    Adicionando e ajustando background.

--- borala/borala/ViewController.swift
+++ borala/borala/ViewController.swift
@@ -10,12 +10,20 @@ import UIKit
 class ViewController: UIViewController {
     
     
+    @IBOutlet weak var backgroundImageView: UIImageView!
+    
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        configElementsBackground()
         
         
-        
-        
+        func configElementsBackground(){
+            
+            backgroundImageView.image = UIImage(named: "background")
+            backgroundImageView.contentMode = .scaleAspectFill
+            
+        }
     }
 }
