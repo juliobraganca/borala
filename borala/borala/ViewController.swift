e0cc9ffd1ccc7b53eba2cb870949d91b121e0294
Eduardo <eduardo07assis@gmail.com>
2023-09-26 23:56:36 -0300

    Criando tela Orçamento.

--- borala/borala/ViewController.swift
+++ borala/borala/ViewController.swift
@@ -9,6 +9,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var backgroundImageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
