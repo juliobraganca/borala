aeba6ddca9421a528c7c18d618df64029ab2e1a9
Eduardo <eduardo07assis@gmail.com>
2023-09-29 04:24:41 -0300

    Created a new cost struct cell.

---
+++ borala/borala/Model/Cost.swift
@@ -0,0 +1,16 @@
+//
+//  Cost.swift
+//  borala
+//
+//  Created by Eduardo Assis on 29/09/23.
+//
+
+import Foundation
+
+struct Cost{
+    
+    var description: String
+    var value: String
+    var check: Bool
+    
+}
