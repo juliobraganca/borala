3436000429efbad34d109da545a9cbdb6272b06e
Eduardo <eduardo07assis@gmail.com>
2023-09-29 04:31:20 -0300

    add.

--- borala/borala/ViewController.swift
+++ borala/borala/ViewController.swift
@@ -8,47 +8,71 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     @IBOutlet weak var backgroundImageView: UIImageView!
     
     @IBOutlet weak var sppentsImageView: UIImageView!
     
+    
     @IBOutlet weak var budgetTableView: UITableView!
+    
+    var list: [Cost] = [Cost(description: "Ingresso Pão de Açucar", value: "56", check: true),
+                        Cost(description: "Ingresso Fla x Vasco", value: "200", check: true),
+                        Cost( description: "Jantar no Oro", value: "500", check: true),
+                        Cost(description: "Uber Urca", value: "45", check: true),
+                        Cost(description: "Açai Ipanema", value: "40", check: true),
+                        Cost(description: "Bike Itau", value: "56", check: true),
+                        Cost(description: "Ingresso Pão de Açucar", value: "100", check: true),
+                        Cost( description: "Jantar no Oro", value: "500", check: true),
+                        Cost(description: "Uber Urca", value: "45", check: true),
+                        Cost(description: "Açai Ipanema", value: "40", check: true),
+                        Cost(description: "Bike Itau", value: "56", check: true),
+                        Cost(description: "Ingresso Pão de Açucar", value: "100", check: true),
+                        
+                        
+    ]
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         configElementsBackground()
         ConfigElementesSppents()
+        configElementsTableView()
     }
-
     
-   func configElementsBackground(){
-       backgroundImageView.image = UIImage(named: "background")
-       backgroundImageView.contentMode = .scaleToFill
+    
+    func configElementsBackground(){
+        backgroundImageView.image = UIImage(named: "background")
+        backgroundImageView.contentMode = .scaleToFill
     }
     
     func ConfigElementesSppents(){
         sppentsImageView.image = UIImage(named: "spents")
         sppentsImageView.contentMode = .scaleToFill
+    }
+    func configElementsTableView(){
+        //budgetTableView.backgroundColor = .systemBackground.self
+        budgetTableView.delegate = self
+        budgetTableView.dataSource = self
+        budgetTableView.register(ExpensesTableViewCell.nib(), forCellReuseIdentifier:ExpensesTableViewCell.identfier)
+    }
     
-        func configElementsTableView(){
-            //budgetTableView.backgroundColor = .systemBackground.self
-            budgetTableView.delegate = self
-            budgetTableView.dataSource = self
-        }
-
-     }
 }
+
 
 extension ViewController: UITableViewDataSource {
     
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 4 //expenses.count ()
+    func tableView(_ budgetTableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return list.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        return UITableViewCell()
+        
+        let Cell = budgetTableView.dequeueReusableCell(withIdentifier: ExpensesTableViewCell.identfier, for: indexPath) as? ExpensesTableViewCell
+        
+        Cell?.setupCell(date: list[indexPath.row])
+        
+        return Cell ?? UITableViewCell()
     }
     
 }
