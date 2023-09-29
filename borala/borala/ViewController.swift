5fa3312de09f511869adc58d2c474a21a8db38a9
Eduardo <eduardo07assis@gmail.com>
2023-09-29 00:13:12 -0300

    chamando configurações das imagens.

--- borala/borala/ViewController.swift
+++ borala/borala/ViewController.swift
@@ -11,10 +11,26 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var backgroundImageView: UIImageView!
     
+    @IBOutlet weak var sppentsImageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        configElementsBackground()
+        ConfigElementesSppents()
     }
 
-
+    
+   func configElementsBackground(){
+       backgroundImageView.image = UIImage(named: "background")
+       backgroundImageView.contentMode = .scaleToFill
+    }
+    
+    func ConfigElementesSppents(){
+        sppentsImageView.image = UIImage(named: "sppent")
+        sppentsImageView.contentMode = .scaleToFill
+        
+  
+         
+     }
 }
 
