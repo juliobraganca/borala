781c602849177a9525d58bd4c9c1053852e19996
Eduardo <eduardo07assis@gmail.com>
2023-10-04 23:46:41 -0300

    no message

---
+++ borala/borala/Budget/Model/Cost.swift
@@ -0,0 +1,17 @@
+//
+//  Cost.swift
+//  borala
+//
+//  Created by Eduardo Assis on 04/10/23.
+//
+
+import Foundation
+
+
+struct Cost{
+    
+    var description: String
+    var value: String
+    var check: Bool
+    
+}
