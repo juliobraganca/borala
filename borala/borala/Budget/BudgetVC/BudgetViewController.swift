80e444bf8ba50762aa7dec170d4a0ab8a7bc8fd3
Eduardo <eduardo07assis@gmail.com>
2023-10-04 23:47:04 -0300

    Revert "no message"
    This reverts commit 781c602849177a9525d58bd4c9c1053852e19996.

--- borala/borala/Budget/BudgetVC/BudgetViewController.swift
+++ borala/borala/Budget/BudgetVC/BudgetViewController.swift
@@ -8,22 +8,102 @@
 import UIKit
 
 class BudgetViewController: UIViewController {
-
+    
+    @IBOutlet weak var backButton: UIButton!
+    
+    @IBOutlet weak var backgroundImageView: UIImageView!
+    
+    @IBOutlet weak var sppentsImageView: UIImageView!
+    
+    @IBOutlet weak var budgetTableView: UITableView!
+    
+    @IBOutlet weak var addbudgetButton: UIButton!
+    
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    @IBOutlet weak var sumButton: UIButton!
+    
+    
+    let newresult: Double = 0
+    
+    
+    var list: [Cost] = [Cost(description: "Ingresso Pão de Açucar", value: "56", check: true),
+                        Cost(description: "Ingresso Fla x Vasco", value: "200", check: true),
+                        Cost( description: "Jantar no Oro", value: "500", check: true),
+                        Cost(description: "Uber Urca", value: "45", check: true),
+                        Cost(description: "Açai Ipanema", value: "40", check: true),
+                      
+    ]
+   
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        configElementsBackground()
+        ConfigElementesSppents()
+        configElementsTableView()
+        resultLabel.text = " R$ \(newresult)"
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    @IBAction func tappedBackButton(_ sender: UIButton) {
+        print("Voltar")
     }
-    */
-
+    
+    @IBAction func tappedAddButton(_ sender: UIButton) {
+        addNewItem()
+        print("Adicionado uma nova celula")
+    }
+    
+    @IBAction func tappedSumButton(_ sender: UIButton) {
+        print("Somar")
+    }
+    
+    func configElementsBackground(){
+        backgroundImageView.image = UIImage(named: "background")
+        backgroundImageView.contentMode = .scaleToFill
+    }
+    
+    
+    func ConfigElementesSppents(){
+        sppentsImageView.image = UIImage(named: "spents")
+        sppentsImageView.contentMode = .scaleToFill
+    }
+    func configElementsTableView(){
+        budgetTableView.backgroundColor = UIColor.clear
+        budgetTableView.delegate = self
+        budgetTableView.dataSource = self
+        budgetTableView.register(ExpensesTableViewCell.nib(), forCellReuseIdentifier:ExpensesTableViewCell.identfier)
+    }
+    
+    func addNewItem(){
+        let newItem = Cost(description: "", value: "", check: true)
+        list.append(newItem)
+        budgetTableView.reloadData()
+        
+       // func configElementsTextField(){
+            
+       // }
+        
+        
+    }
 }
+
+extension BudgetViewController: UITableViewDataSource {
+    
+    func tableView(_ budgetTableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return list.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let Cell = budgetTableView.dequeueReusableCell(withIdentifier: ExpensesTableViewCell.identfier, for: indexPath) as? ExpensesTableViewCell
+        
+        Cell?.setupCell(date: list[indexPath.row])
+        
+        return Cell ?? UITableViewCell()
+    }
+    
+}
+extension BudgetViewController: UITableViewDelegate {
+//
+
+  }
