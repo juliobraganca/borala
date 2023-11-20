df680c267e5ee3ea6ed35e313a3a6872243a9754
Julio <jcbbraganca@gmail.com>
2023-11-20 15:19:52 -0300

    Alterando célula tanto layout quanto inclusão do check

--- borala/borala/Budget/CellExpenses/ExpensesTableViewCell.swift
+++ borala/borala/Budget/CellExpenses/ExpensesTableViewCell.swift
@@ -10,8 +10,7 @@ import UIKit
 class ExpensesTableViewCell: UITableViewCell {
     
     
-   
-    @IBOutlet weak var chackButton: UIButton!
+    @IBOutlet weak var checkButton: UIButton!
     
     @IBOutlet weak var descriptionTextField: UITextField!
  
@@ -19,26 +18,28 @@ class ExpensesTableViewCell: UITableViewCell {
     
     static let identfier: String = "ExpensesTableViewCell"
     
+    let backColor: UIColor? = Color.neutral
+    
     static func nib() -> UINib {
         return UINib(nibName: identfier, bundle: nil)
-        
     }
     
     override func awakeFromNib() {
         super.awakeFromNib()
-    
     }
-
-    func setupCell(date: Cost){
-        descriptionTextField.text = date.description
-        valueTextField.text = date.value
-        if date.check {
-            chackButton.tintColor = .blue
-        }else {
-            chackButton.tintColor = .systemPink
-
-        }
+    
+    @IBAction func tappedCheckButton(_ sender: Any) {
+        checkButton.setImage(UIImage(systemName: "checkmark.square"), for: .normal)
+    }
+    
+    func setupCell(data: Cost){
+        descriptionTextField.text = data.description
+        valueTextField.text = String(data.value)
+        checkButton.setImage(UIImage(systemName: "square"), for: .normal)
         
+        
+        descriptionTextField.backgroundColor = backColor
+        valueTextField.backgroundColor = backColor
+//        chackButton.backgroundColor = backColor
     }
-    
 }
