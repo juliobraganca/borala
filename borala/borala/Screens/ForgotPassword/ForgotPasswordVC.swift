e4af2b88201fb86fb925843f7610b12d1e87feda
Eduardo <eduardo07assis@gmail.com>
2023-11-22 09:53:27 -0300

    Adicionando botão de retorno para tela de login, removendo itens comentados e  ajustes na screen.

--- borala/borala/Screens/ForgotPassword/ForgotPasswordVC.swift
+++ borala/borala/Screens/ForgotPassword/ForgotPasswordVC.swift
@@ -6,13 +6,14 @@
 //
 
 import UIKit
-import FirebaseAuth
 import Firebase
 
 class ForgotPasswordVC: UIViewController {
     
     
     
+    @IBOutlet weak var returnButton: UIButton!
+    @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var logoImageView: UIImageView!
     @IBOutlet weak var emailTextField: UITextField!
     @IBOutlet weak var confirmButton: UIButton!
@@ -20,65 +21,53 @@ class ForgotPasswordVC: UIViewController {
     var auth: Auth?
     override func viewDidLoad() {
         super.viewDidLoad()
-        //        configureVC()
-        configureImageView()
-        configureTextField()
         configureButton()
+        configureImageView()
+        configElementes()
         self.auth = Auth.auth()
     }
-    
-    
-    //    func configureVC() {
-    //        view.setBackground()
-    //    }
-    
     
     func configureImageView() {
         logoImageView.image = Images.logo
     }
     
-    
-    func configureTextField() {
-        //        emailTextField.setupTextField(placeholder: "Insira seu email", delegate: self)
+    func configElementes() {
         emailTextField.placeholder = "Digite seu email"
         emailTextField.keyboardType = .emailAddress
         emailTextField.delegate = self
         
+        navigationController?.navigationBar.isHidden = true
+        navigationController?.navigationBar.barStyle = .black
+        view.backgroundColor = .systemGray5
+        
+        titleLabel.text = " Recuperar Senha"
+        titleLabel.textColor = .black
+
     }
-    
     
     func configureButton() {
         confirmButton.setTitle("Enviar", for: .normal)
         confirmButton.tintColor = UIColor.black
-        
     }
-    
-    
+  
+    @IBAction func tappedReturnButton(_ sender: Any) {
+        self.navigationController?.popViewController(animated: false)
+    }
     @IBAction func tappedConfirmButton(_ sender: UIButton) {
-        
-        let email:String = emailTextField.text ?? ""
-        
-        if let text = emailTextField.text, text.isEmpty {
-            self.showAlert(title: "Erro!" , message: "Ops! Parece que o campo de email está vazio. Preencha e tente novamente.")
-            
-            
-        } else {
-            
+        if self.emailTextField.validateEmail() {
+            let email:String = emailTextField.text ?? ""
             auth?.sendPasswordReset(withEmail: email) { (error) in
-                
-                if let error = error {
-                    self.showAlert(title: "Erro!" , message: "Erro ao enviar email de redefinição de senha: \(error.localizedDescription)")
-                    
+                if error != nil {
+                    self.showalertError(title: "Erro!" , message: "Erro ao enviar email de redefinição de senha.")
                 } else {
-                    self.showAlert(title: "Sucesso!" , message: "Email de redefinição de senha enviado com sucesso. Verifique sua caixa de entrada.")
-                   
-                                   }
-                
+                    self.showAlert(title: "Sucesso!" , message: "Email de redefinição de senha enviado com sucesso. Verifique sua caixa de entrada!")
+                }
             }
+        } else {
+            self.showalertError(title: "Erro!" , message: "Verifique os dados inseridos e tente novamente!")
         }
     }
 }
-    
 
 extension ForgotPasswordVC: UITextFieldDelegate {
     
@@ -86,17 +75,24 @@ extension ForgotPasswordVC: UITextFieldDelegate {
         textField.isEditingTextField()
     }
     
-    
     func textFieldDidEndEditing(_ textField: UITextField) {
         textField.isTextFieldEmpty()
-        //enableButton()
     }
-    
     
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         textField.resignFirstResponder()
         return true
+        
     }
+    
+    func showalertError(title:String, message:String ){
+        let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert )
+        let okButton: UIAlertAction = UIAlertAction(title: "OK", style: .default)
+        alertController.addAction(okButton)
+        self.present(alertController,animated: true, completion: nil)
+    }
+    
+    
     func showAlert(title:String, message: String) {
         let alertController: UIAlertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
         let alertControllerReturnLogin: UIAlertAction = UIAlertAction(title: "OK", style: .default) {(action) in
@@ -105,20 +101,11 @@ extension ForgotPasswordVC: UITextFieldDelegate {
         alertController.addAction(alertControllerReturnLogin)
         self.present(alertController,animated: true, completion: nil)
     }
+    
     func redirectForLogin(){
         if let loginVC = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "LoginVC") as? LoginVC {
             self.navigationController?.pushViewController(loginVC, animated: true)
         }
     }
-        
-        //
-        //    func enableButton() {
-        //        if emailTextField.hasText {
-        //            confirmButton.isEnabled = true
-        //        } else {
-        //            confirmButton.isEnabled = false
-        //        }
-        //    }
-        
-    }
-    
+}
+
